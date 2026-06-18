72174f3ca4d63cffeee2580a89c9837bd07cda8f
Soumya <aymousbeatz@gmail.com>
2026-06-18 21:48:43 +0530

    Add file class with data handling methods
    Implemented a basic file class with methods for data insertion and reading. Added an example usage of the file class.

--- file-sys.py
+++ file-sys.py
@@ -1,1 +1,26 @@
+# file-system:
+# a virtual system that illustates the file-system
+
+# definition of file class
+class file:
+    def __init__ (self, name, _type, data=None):
+        self.name = name
+        self._type = _type
+        
+    def __str__(self):
+        return f"{self.name}.{self._type}"
+        
+    
+    def data_insert(self, data):
+        self.data = data
+        
+    def read_data(self):
+        return self.data
+        
+
+# Example of file:
+temp1 = file("temp1", "txt")
+temp1.data_insert("this is just a temp file..")
+print(f"File name: {temp1}")
+print(temp1.read_data())
 

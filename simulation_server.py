bd35a166d342579bfd6dfc6452017fe8eaa66a22
Soumya <aymousbeatz@gmail.com>
2026-06-29 13:57:38 +0530

    Implement simulation server that prints random binary grid

---
+++ simulation_server.py
@@ -0,0 +1,26 @@
+import random
+
+def server():
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+    print(f"|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|{random.randint(0,1)}|")
+    print("-----------")
+
+
+while True:
+    server()

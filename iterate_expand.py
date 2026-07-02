220094c349cb4cbe387b525882eda40ee04cfb87
Soumya <aymousbeatz@gmail.com>
2026-07-02 19:21:03 +0530

    Implement list comprehensions for powers of numbers

---
+++ iterate_expand.py
@@ -0,0 +1,11 @@
+# Print i * i for 10:
+arr1 = [i * i for i in range(1, 10 + 1)]
+print(arr1)
+
+# Print i * i * i for 10:
+arr2 = [i * i * i for i in range(1, 10 + 1)]
+print(arr2)
+
+# Print i * i * i * i for 10:
+arr3 = [i * i * i * i for i in range(1, 10 + 1)]
+print(arr3)

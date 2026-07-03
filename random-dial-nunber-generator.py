ed0001d76f716370f8d10b4d6d2b643453b0514e
Soumya <aymousbeatz@gmail.com>
2026-07-03 12:14:13 +0530

    Implement phone number generator in Python script

---
+++ random-dial-nunber-generator.py
@@ -0,0 +1,3 @@
+import random as r
+print("PHONE NUMBER GENERATED:")
+print(f"{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}{r.randint(1, 9)}")
